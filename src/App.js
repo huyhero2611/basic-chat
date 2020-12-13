28818519b35f63ace4ce6c795e7e087d059ff5f8
フイ <48904215+huyhero2611@users.noreply.github.com>
2020-12-13 22:24:24 +0700

    create app chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  // your config
+  apiKey: "AIzaSyCNPSsg-1d3yzn92-YfEQOc72_7zJBPk4I",
+  authDomain: "basic-chat-dd233.firebaseapp.com",
+  projectId: "basic-chat-dd233",
+  storageBucket: "basic-chat-dd233.appspot.com",
+  messagingSenderId: "316947476178",
+  appId: "1:316947476178:web:70305c07dbc644d546c70b",
+  measurementId: "G-0Z4825BVE0",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{user ? <ChatRoom /> : <SignIn />}</header>
     </div>
   );
 }
 
 export default App;
+
+function SignIn() {
+  return <button onClick={signInWithGoogle}>Sign In with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const messageRef = firestore.collection;
+}
